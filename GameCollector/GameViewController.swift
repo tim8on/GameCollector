0150a74dfb49412d689a1bce3f504c0a3490ee40
tim8on <tim8on@gmail.com>
2018-06-05 15:49:53 -0700

    added UI elements

---
+++ GameCollector/GameViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GameViewController.swift
+//  GameCollector
+//
+//  Created by Tim Aton on 6/5/18.
+//  Copyright © 2018 Tim Aton. All rights reserved.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+
+    @IBOutlet weak var gameImageView: UIImageView!
+    @IBOutlet weak var titleTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func photosTapped(_ sender: Any) {
+    }
+    
+    @IBAction func cameraTapped(_ sender: Any) {
+    }
+    
+    @IBAction func addTapped(_ sender: Any) {
+    }
+}
