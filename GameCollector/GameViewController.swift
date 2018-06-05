47b406b19054fb467f51c073e4b39afe15240e10
tim8on <tim8on@gmail.com>
2018-06-05 16:26:29 -0700

    grab an image from user's photos

--- GameCollector/GameViewController.swift
+++ GameCollector/GameViewController.swift
@@ -30,6 +30,13 @@ class GameViewController: UIViewController, UIImagePickerControllerDelegate, UIN
         present(imagePicker, animated: true, completion: nil)
     }
     
+    //set the image that the user chose to the game image
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        gameImageView.image = image
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
     @IBAction func cameraTapped(_ sender: Any) {
     }
     
